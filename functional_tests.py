0d28f5147f05c6b793c19a05454c39bfb1749cc9
Sam Bird <sxb1413@student.bham.ac.uk>
2020-07-02 18:50:14 +0100

    Updated functional test to redirect to CV page, updated view and url to pass this, then updated functional test to include verifying that the form exists and made this pass by creating form in cv.html

--- functional_tests.py
+++ functional_tests.py
@@ -20,11 +20,31 @@ class CreateCVTest(unittest.TestCase):
     	self.assertIn("CV", (self.browser.find_element_by_id('cvLink').text))
 
     	# He clicks the CV button and it takes him to a new page
-    	# This page will display the CV and allow Q to edit it if he has admin access by pressing the edit button at the top of the page
+    	self.browser.find_element_by_id('cvLink').click();
+    	self.assertIn("http://localhost:8000/cv.html", self.browser.current_url)
+
+    	# This page will display the CV and allow Q to input information into the CV
     	# A form with fields for name, phone number, email address, intro, education, experience, skills and interests appears on the page
+    	name_input = self.browser.find_element_by_id('cv_name')
+    	self.assertIn("Name", name_input.get_attribute('placeholder'))
+    	phone_input = self.browser.find_element_by_id('cv_phone')
+    	self.assertIn("Phone Number", phone_input.get_attribute('placeholder'))
+    	email_input = self.browser.find_element_by_id('cv_email')
+    	self.assertIn("Email Address", email_input.get_attribute('placeholder'))
+    	intro_input = self.browser.find_element_by_id('cv_intro')
+    	self.assertIn("Intro", intro_input.get_attribute('placeholder'))
+    	education_input = self.browser.find_element_by_id('cv_education')
+    	self.assertIn("Education", education_input.get_attribute('placeholder'))
+    	experience_input = self.browser.find_element_by_id('cv_experience')
+    	self.assertIn("Experience", experience_input.get_attribute('placeholder'))
+    	skills_input = self.browser.find_element_by_id('cv_skills')
+    	self.assertIn("Skills", skills_input.get_attribute('placeholder'))
+    	interests_input = self.browser.find_element_by_id('cv_interests')
+    	self.assertIn("Interests", interests_input.get_attribute('placeholder'))
+
     	# After filling in these boxes he notices a save button at the bottom of the screen
     	# He presses the save button
-    	# This redirects him to the view of the CV, which should now be filled with the information he just entered.
+    	# The CV should now have been updated with the information that Q has just entered
 
 
 if __name__ == '__main__':  
